fb4acc07212b561fe1fc556f1245f4a383e0124a
Chaitanya Baweja <chaitanya.baweja17@imperial.ac.uk>
2020-03-20 00:47:37 +0530

    Adding message for input

--- COVID_Mail/covid_mail.py
+++ COVID_Mail/covid_mail.py
@@ -21,7 +21,7 @@ import schedule
 import time
 
 # take input initially and setup driver once
-Country = input() 
+Country = input('Entry Country Name: ') 
 driver = webdriver.Chrome('path/to/chromedriver')
 
 def extract_data(Country_name):
